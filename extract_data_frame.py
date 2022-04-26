3115a4f3ae49609b130bd6a621b01e7e04763651
Caleb Omariba <98550214+calebomariba@users.noreply.github.com>
2022-04-26 06:42:13 +0300

    initialized the class

--- extract_data_frame.py
+++ extract_data_frame.py
@@ -6,4 +6,6 @@ class TweetExtractor:
     """
     this class extracts tweets it receives in purses them into  a pandas datframe
     """
-    
+    def __init__(self, tweetslist):
+        
+        self.tweetslist = tweetslist
