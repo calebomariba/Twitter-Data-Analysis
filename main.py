72092b89e52e95b8386bda554e04acecc11ff84a
Caleb Omariba <98550214+calebomariba@users.noreply.github.com>
2022-04-29 13:23:09 +0300

    did data and text processing

--- main.py
+++ main.py
@@ -29,3 +29,28 @@ tweets_df.dropna()
 if st.sidebar.checkbox("show concise summary"):
     st.write(tweets_df)
 
+
+#########################
+# data preprocessing
+#######################
+class PrepareData:
+  def __init__(self,df):
+    self.df=df
+    
+  def preprocess_data(self):
+    tweets_df = self.df.loc[self.df['lang'] =="en"]
+
+    
+    #text Preprocessing
+    tweets_df['clean_text']=tweets_df['clean_text'].astype(str)
+    tweets_df['clean_text'] = tweets_df['clean_text'].apply(lambda x: x.lower())
+    tweets_df['clean_text']= tweets_df['clean_text'].apply(lambda x: x.translate(str.maketrans(' ', ' ', string.punctuation)))
+    
+    #Converting tweets to list of words For feature engineering
+    sentence_list = [tweet for tweet in tweets_df['clean_text']]
+    word_list = [sent.split() for sent in sentence_list]
+
+    #Create dictionary which contains Id and word 
+    word_to_id = corpora.Dictionary(word_list)
+    corpus_1= [word_to_id.doc2bow(tweet) for tweet in word_list]
+
