666d411ca74ea757ee0bacb2176fc69adfc08d16
Caleb Omariba <98550214+calebomariba@users.noreply.github.com>
2022-04-29 16:49:46 +0300

    ploted top 20 hashtags

--- extract_data_frame.py
+++ extract_data_frame.py
@@ -1,6 +1,6 @@
 import json
 import pandas as pd
-from textblob import TextBlob
+# from textblob import TextBlob
 
 def read_json(json_file: str)->list:
     
@@ -14,7 +14,7 @@ def read_json(json_file: str)->list:
     
 class TweetExtractor:
     
-    def _init_(self, tweets_list):
+    def __init__(self, tweets_list):
         
         self.tweets_list = tweets_list
 
@@ -24,23 +24,21 @@ class TweetExtractor:
         return statuses_count
         
     def find_full_text(self)->list:
-        text = []
-        for tweet in self.tweets_list:
-            if 'retweeted_status' in tweet.keys() and 'extended_tweet' in tweet['retweeted_status'].keys():
-                text.append(tweet['retweeted_status']['extended_tweet']['full_text'])
-            else: text.append('Empty')
+        lang = [x['text'] for x in self.tweets_list]
+        
+        return lang
 
-        return text       
     
-    def find_sentiments(self, text:list)->list:
-        polarity, subjectivity = [], []
-        for tweet in text:
-            blob = TextBlob(tweet)
-            sentiment = blob.sentiment
-            polarity.append(sentiment.polarity)
-            subjectivity.append(sentiment.subjectivity)
+    # def find_sentiments(self, text:list)->list:
+        # polarity, subjectivity = [], []
+        # for tweet in text:
+        #     blob = TextBlob(tweet)
+        #     sentiment = blob.sentiment
+        #     polarity.append(sentiment.polarity)
+        #     subjectivity.append(sentiment.subjectivity)
 
-        return polarity, subjectivity
+        # return polarity, subjectivity
+
 
     def find_created_time(self)->list:
         created_at = [x['created_at'] for x in self.tweets_list]
@@ -129,13 +127,13 @@ class TweetExtractor:
     def get_tweet_df(self, save=False)->pd.DataFrame:
         """required column to be generated you should be creative and add more features"""
         
-        columns = ['created_at', 'source', 'original_text','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
+        columns = ['created_at', 'source', 'original_text','lang', 'favorite_count', 'retweet_count', 
             'original_author', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place']
         
         created_at = self.find_created_time()
         source = self.find_source()
         text = self.find_full_text()
-        polarity, subjectivity = self.find_sentiments(text)
+        # polarity, subjectivity = self.find_sentiments(text)
         lang = self.find_lang()
         fav_count = self.find_favourite_count()
         retweet_count = self.find_retweet_count()
@@ -146,7 +144,7 @@ class TweetExtractor:
         hashtags = self.find_hashtags()
         mentions = self.find_mentions()
         location = self.find_location()
-        data = zip(created_at, source, text, polarity, subjectivity, lang, fav_count, retweet_count, screen_name, follower_count, friends_count, sensitivity, hashtags, mentions, location)
+        data = zip(created_at, source, text, lang, fav_count, retweet_count, screen_name, follower_count, friends_count, sensitivity, hashtags, mentions, location)
         df = pd.DataFrame(data=data, columns=columns)
         
         if save:
